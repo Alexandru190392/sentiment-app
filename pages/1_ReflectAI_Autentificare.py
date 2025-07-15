2dcf8bf19ee66a52d401a7f9574c16d4dcb68b43
Alexandru190392 <1alexandru.florin.draghici@gmail.com>
2025-07-15 16:31:44 +0300

    Update 1_ReflectAI_Autentificare.py

--- pages/1_ReflectAI_Autentificare.py
+++ pages/1_ReflectAI_Autentificare.py
@@ -74,7 +74,7 @@ if "utilizator" in st.session_state and st.session_state.get("redirectat", False
     st.session_state["redirectat"] = True
     with st.spinner("Te conectezi la aplicație..."):
         time.sleep(1)  # mică pauză pentru UX
-    switch_page("ReflectAI")  # ← Schimbă aici în funcție de numele fișierului din /pages/
+   switch_page("2_Jurnal_Emotional") # ← Schimbă aici în funcție de numele fișierului din /pages/
 
 # === Status sesiune ===
 if "utilizator" in st.session_state:
