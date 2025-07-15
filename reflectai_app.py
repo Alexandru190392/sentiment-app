b9bd5b06b09408344a80a191118a7540fcd4434b
Alexandru190392 <1alexandru.florin.draghici@gmail.com>
2025-07-15 15:32:35 +0300

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -2,10 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="ReflectAI", page_icon="🧠", layout="centered")
 
-st.title("🧠 ReflectAI – Jurnal Emoțional")
-
-if "utilizator" in st.session_state:
-    st.success(f"Bine ai revenit, {st.session_state['utilizator']}!")
-    st.markdown("📓 Poți accesa jurnalul tău emoțional din bara din stânga.")
-else:
-    st.info("🔐 Te rugăm să te autentifici din pagina de **Autentificare** din meniul din stânga.")
+st.title("🧠 sentiment app")
+st.info("🔐 Te rugăm să te autentifici din pagina de 'ReflectAI Autentificare'.")
